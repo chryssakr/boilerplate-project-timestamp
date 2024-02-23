ab61f48310627376f95ff8785fcd7f2a533af899
Chryssa Karatza <chryssakaratza@gmail.com>
2024-02-23 18:36:17 +0000

    work on api

--- index.js
+++ index.js
@@ -18,10 +18,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+function isInvalidDate(date) {
+  return date.toUTCString() === "Invalid Date";
+}
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/views/api/:date", function (req, res) {
+  let date = new Date(req.params.date)
+
+  if (isInvalidDate(date)) {
+    date = new Date(+req.params.date) // in case it's a string
+  }
+
+  if(isInvalidDate(date)){
+    res.json({error: "Invalid Date"})
+    return;
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
 });
 
 
