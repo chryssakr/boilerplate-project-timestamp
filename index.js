8f98df509d533c001ab862662c66e106664c8a4b
Chryssa Karatza <chryssakaratza@gmail.com>
2024-03-07 16:02:38 +0000

    finish api

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+const PORT = 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -23,7 +24,7 @@ function isInvalidDate(date) {
 }
 
 // your first API endpoint... 
-app.get("/views/api/:date", function (req, res) {
+app.get("/api/:date", function (req, res) {
   let date = new Date(req.params.date)
 
   if (isInvalidDate(date)) {
@@ -41,9 +42,7 @@ app.get("/views/api/:date", function (req, res) {
   });
 });
 
-
-
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT || PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
